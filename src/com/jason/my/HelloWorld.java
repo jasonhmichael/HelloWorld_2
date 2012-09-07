bc49197e9c350a61363940a185cf458718c69c5c
Jason Michael <skidyid@yahoo.com>
2012-09-06 22:17:58 -0500

    Some commit

---
+++ src/com/jason/my/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.jason.my;
+
+import java.util.Iterator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: U0115030
+ * Date: 9/6/12
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World.  Jason rules.");
+        // nothing else
+    }
+}
